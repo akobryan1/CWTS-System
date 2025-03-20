7f2a1cb4e65e1aaa0654bf80453648f265fc39d9
akobryan1 <71861402+akobryan1@users.noreply.github.com>
2025-03-20 09:43:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,24 @@ function enforceNumericInput(event) {
     event.target.value = value.replace(/\D/g, '');
 }
 
+// Firebase SDK Imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
+
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD8qK7GrYlOj_NFWaSTD8xhWYdCmoYrWzo",
+  authDomain: "cwts-system.firebaseapp.com",
+  projectId: "cwts-system",
+  storageBucket: "cwts-system.firebasestorage.app",
+  messagingSenderId: "1022635923004",
+  appId: "1:1022635923004:web:57b050e34840ba9c6e1539",
+  measurementId: "G-SCWVRFQ9Y6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
